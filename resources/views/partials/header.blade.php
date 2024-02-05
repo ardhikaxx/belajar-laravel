31dc10f097d2b743d2dca1a3b75537aaf88e56b8
ardhikaxx <ardhikayanuar58@gmail.com>
2024-02-05 20:30:01 +0700

    upload pertama

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<section class="py-5">
+	<nav class="navbar navbar-expand-md bg-light">
+		<div class="container">
+			<a class="navbar-brand" href=""><img alt="Free Frontend Logo" class="img-fluid" height="" src="https://freefrontend.dev/wp-content/uploads/free-frontend-logo.png" width="300"></a> <button aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-bs-target="#navbarSupportedContent1" data-bs-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent1">
+				<ul class="navbar-nav ms-auto my-2 my-md-0">
+					<li class="nav-item ms-lg-4">
+						<a class="nav-link" href="{{ route('pages.index') }}">Dashboard</a>
+					</li>
+					<li class="nav-item ms-lg-4">
+						<a class="nav-link" href="{{ route('pages.about') }}">About</a>
+					</li>
+					<li class="nav-item ms-lg-4">
+						<a class="nav-link" href="">Blog</a>
+					</li>
+					<li class="nav-item ms-lg-4">
+						<a class="nav-link" href="">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</section>
